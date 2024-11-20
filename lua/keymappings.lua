995946e2bd1a93306465a38b18abc102f2b0a804
Alex Cantu <miguel.can2@gmail.com>
2024-11-20 15:32:56 -0600

    Update keymappings to use wk.add

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,46 +1,47 @@
 local utils = require('utils')
+local wk = require('which-key')
 
--- Neotree
-utils.map('i', [[<c-n>]], ':NvimTreeToggle<cr>')
-utils.map('n', [[<c-n>]], ':NvimTreeToggle<cr>')
+-- NvimTree
+wk.add({
+  { "<c-n>", "<cmd>NvimTreeToggle<cr>", desc = "Code Action", mode = {"i", "n" }},
+})
 
 -- bufferline
-utils.map('n', [[<leader>bl]], ':BufferLinePick<CR>')
--- ufo code folding
-vim.keymap.set('n', 'zN', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
--- Neogit
--- utils.map('n', [[<leader>g]], ':Neogit kind=vsplit<CR>')
+wk.add({
+  { "<leader>b", group = "BufferLine" },
+  { "<leader>bl", "<cmd>BufferLinePick<cr>", mode = "n" },
+})
 
 -- DiffviewFile
-utils.map('n', [[<leader>df]], ':DiffviewFile %s<CR>')
-
--- Obsidian
-utils.map('n', '<leader>oo', '<cmd>ObsidianOpen<cr>')
-utils.map('n', '<leader>on', '<cmd>ObsidianNew<cr>')
-utils.map('n', '<leader>oT', '<cmd>ObsidianTemplate<cr>')
-utils.map('n', '<leader>ot', '<cmd>ObsidianToday<cr>')
-utils.map('n', '<leader>oy', '<cmd>ObsidianYesterday<cr>')
-utils.map('n', '<leader>ol', '<cmd>ObsidianLink<cr>')
-utils.map('n', '<leader>oL', '<cmd>ObsidianLinkNew<cr>')
-utils.map('n', '<leader>ob', '<cmd>ObsidianBacklinks<cr>')
-utils.map('n', '<leader>os', '<cmd>ObsidianSearch<cr>')
-utils.map('n', '<leader>oq', '<cmd>ObsidianQuickSwitch<cr>')
+wk.add({
+  { "<leader>d", group = "Diffview" },
+  {
+    "<leader>df",
+    "<cmd>DiffviewFileHistory % --no-merges<cr>",
+    mode = "n",
+    desc = "View the git history of the current buffer",
+  },
+  {
+    "<leader>dq",
+    "<cmd>DiffviewClose<cr>",
+    mode = "n",
+    desc = "Close DiffviewFileHistory view",
+  },
+})
 
 -- Open coc settings
 vim.keymap.set('n', '<space>1', ":vsplit ~/.config/nvim/lua/coc.lua<cr>", { silent = true, nowait = true })
 
 -- toggle term
-function _G.set_terminal_keymaps()
-  local opts = { noremap = true }
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-end
+-- function _G.set_terminal_keymaps()
+--   local opts = { noremap = true }
+--   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+-- end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-utils.map('i', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm size=80 direction=vertical"<CR>')
-utils.map('n', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm size=80 direction=vertical"<CR>')
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+-- utils.map('i', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm size=80 direction=vertical"<CR>')
+-- utils.map('n', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm size=80 direction=vertical"<CR>')
 -- local Terminal = require('toggleterm.terminal').Terminal
 -- local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
@@ -51,11 +52,24 @@ utils.map('n', [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm size=80 direction=ver
 -- vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
 -- treesj
--- For use default preset and it work with dot
-vim.keymap.set('n', '<leader>s', require('treesj').toggle)
+wk.add({
+  { "<leader>s", require('treesj').toggle, mode = "n" },
+})
 
---ssr
-vim.keymap.set({ "n", "x" }, "<leader>sr", function() require("ssr").open() end)
+-- ssr and spectre
+wk.add({
+  { "<leader>s", group = "Search and Replace" },
+  { "<leader>S", require("spectre").toggle, mode = "n", desc = "Toggle Spectre"},
+  { "<leader>sw", function() require("spectre").open_visual({select_word = true}) end, mode = "n", desc = "Search Current Word"},
+  { "<leader>sw", require("spectre").open_visual, mode = "v", desc = "Search Current Word"},
+  { "<leader>sf", function() require("spectre").open_file_search({select_word = true}) end, mode = "n", desc = "Search on current file"},
+  {
+    "<leader>sr",
+    require("ssr").open,
+    mode = { "n", "x" },
+    desc = "Structural Search and Replace",
+  },
+})
 
 -- vim-vsnip
 vim.cmd("imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'")
@@ -65,28 +79,7 @@ vim.cmd("smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'    
 vim.cmd("imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'")
 vim.cmd("smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'")
 
--- harpoon
-vim.keymap.set('n', 'hx', require('harpoon.mark').add_file)
-vim.keymap.set('n', 'hn', require('harpoon.ui').nav_next)
-vim.keymap.set('n', 'hp', require('harpoon.ui').nav_prev)
-utils.map('n', 'hm', ':telescope harpoon marks<cr>')
-
--- spectre
-vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
-  desc = "Toggle Spectre"
-})
-vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-  desc = "Search current word"
-})
-vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-  desc = "Search current word"
-})
-vim.keymap.set('n', '<leader>sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-  desc = "Search on current file"
-})
-
 -- gp (Chat GPT)
-local wk = require('which-key')
 wk.add({
   { "<leader>u", group = "ChatGPT" },
   { "<leader>uc", "<cmd>GpChatToggle vsplit<CR>", desc = "Toggle Chat" },
@@ -136,6 +129,11 @@ local function visual_cursors_with_delay()
 end
 
 -- visual-multi
+-- I need to figure out how to migrate this over to the wk.add method.
+-- Since visual multi uses Plug instead of <cmd>, the invocation will
+-- look a little different
+-- Actually, the reason I need this is because I use the visual_cursors_with_delay
+-- function as part of the <leader>mv mapping
 wk.register({
   m = {
     name = "Visual Multi",
