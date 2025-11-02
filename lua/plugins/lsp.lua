d9a7f3a6bead8f26b4973dff2b87ba9f23e245d7
Alex Cantu <miguel.can2@gmail.com>
2025-11-02 16:07:31 -0600

    Add Mason and LSP configuration with vim.lsp.config API
    - Install Mason.nvim (v2.1.0) for LSP server management
    - Add mason-lspconfig for automatic LSP server setup
    - Configure lua_ls with vim.lsp.config API (Neovim 0.11+)
    - Register LSP keymaps with which-key under <leader>l prefix
    - Add Mason installer command under <leader>lm
    - Update lazy-lock.json with new dependencies

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,89 @@
 -- LSP configuration and related plugins
 
 return {
-  -- LSP Configuration
-  -- Example:
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   dependencies = {
-  --     "williamboman/mason.nvim",
-  --     "williamboman/mason-lspconfig.nvim",
-  --   },
-  --   config = function()
-  --     require("mason").setup()
-  --     require("mason-lspconfig").setup({
-  --       ensure_installed = { "lua_ls", "ts_ls" },
-  --     })
-  --
-  --     local lspconfig = require("lspconfig")
-  --     local capabilities = require("cmp_nvim_lsp").default_capabilities()
-  --
-  --     lspconfig.lua_ls.setup({ capabilities = capabilities })
-  --     lspconfig.ts_ls.setup({ capabilities = capabilities })
-  --   end,
-  -- },
+  -- Mason: Portable package manager for LSP servers, DAP servers, linters, and formatters
+  {
+    "williamboman/mason.nvim",
+    version = "v2.1.0",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    opts = {
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>lm", "<cmd>Mason<CR>", desc = "Mason installer" },
+      })
+    end,
+  },
+
+  -- Mason-LSPConfig: Bridge between mason.nvim and nvim-lspconfig
+  -- Automatically enables installed servers via vim.lsp.enable()
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls" },
+      automatic_installation = true,
+    },
+  },
+
+  -- LSP Configuration using vim.lsp.config API (Neovim 0.11+)
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      -- Configure lua_ls using vim.lsp.config API
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+
+      -- Enable lua_ls
+      vim.lsp.enable("lua_ls")
+
+      -- Register LSP keymaps with which-key
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>l", group = "LSP" },
+        { "<leader>la", vim.lsp.buf.code_action, desc = "Code action" },
+        { "<leader>lr", vim.lsp.buf.rename, desc = "Rename" },
+        { "<leader>lf", vim.lsp.buf.format, desc = "Format" },
+        { "<leader>ld", vim.lsp.buf.definition, desc = "Go to definition" },
+        { "<leader>lD", vim.lsp.buf.declaration, desc = "Go to declaration" },
+        { "<leader>li", vim.lsp.buf.implementation, desc = "Go to implementation" },
+        { "<leader>lt", vim.lsp.buf.type_definition, desc = "Type definition" },
+        { "<leader>lR", vim.lsp.buf.references, desc = "References" },
+        { "K", vim.lsp.buf.hover, desc = "Hover documentation" },
+        { "<C-k>", vim.lsp.buf.signature_help, desc = "Signature help", mode = "i" },
+      })
+    end,
+  },
 }
+
